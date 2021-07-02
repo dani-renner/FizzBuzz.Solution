6a3c7f14939ac56bc8268a1bb704bf2e11f993a5
Dani Renner <danijrenner@gmail.com>
2021-07-01 17:51:44 -0700

    add code, empty test runs

---
+++ FizzBuzz.Tests/FizzBuzzTests.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using FizzBuzz;
+
+namespace FizzBuzzTests
+{
+    [TestClass]
+    public class FizzBuzzTester
+    {
+        [TestMethod]
+        public void FizzBuzzMethod_MultiplesOfFifteen_FizzBuzz()
+        {
+        }
+    }
+}
